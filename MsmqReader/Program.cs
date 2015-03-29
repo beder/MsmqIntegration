26e90adb38bf8c31d7f2ed5db41ecfc5495e5ee2
Beder Acosta Borges <bedermail@yahoo.com>
2015-03-29 12:22:11 +0200

    simple writer/reader projects

---
+++ MsmqReader/Program.cs
@@ -0,0 +1,27 @@
+﻿using MsmqCommon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MsmqReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageQueue messageQueue = new MessageQueue(".\\Private$\\Notifications");
+
+            messageQueue.Formatter = new XmlMessageFormatter(new System.Type[]
+                {
+                    new Notification().GetType(),
+                    new Object().GetType()
+                });
+
+            Notification notification = (Notification)messageQueue.Receive().Body;
+            Console.WriteLine("Processing {0} ", notification);
+        }
+    }
+}
---
+++ MsmqWriter/Program.cs
@@ -0,0 +1,29 @@
+﻿using MsmqCommon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MsmqWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notification notification;
+            notification.NotificationType = "MsmqWriter";
+            notification.Payload = "Written by MsmqWriter on " + DateTime.Now.ToString();
+
+            Message message = new Message();
+            message.Body = notification;
+
+            using (MessageQueue messageQueue = new MessageQueue(".\\Private$\\Notifications"))
+            {
+                messageQueue.Send(message);
+                messageQueue.Close();
+            }
+        }
+    }
+}
